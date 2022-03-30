4bd39dbaafcc2e8268da66d861b2689b9d52fa71
Edwin Villacis <edwinvillacisfeus@yahoo.com>
2022-03-29 22:33:22 -0500

    GraphQL - Node.js for my YouTube channel

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * GraphQL - Node.js for my YouTube channel
+ */
+
+ import express = require("express");
+
+ const app = express();
+ 
+ app.listen(3000, () => {
+   console.log("App is running on PORT 3000");
+ });
+ 
