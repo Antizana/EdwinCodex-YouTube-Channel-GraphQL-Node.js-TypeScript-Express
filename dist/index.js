affd94c45e8f785163ad21a9fa6d24de78c7eac6
Edwin Villacis <edwinvillacisfeus@yahoo.com>
2022-03-29 22:51:09 -0500

    GraphQL - Node.js - Express - Typescript for my YouTube channel

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * GraphQL - Node.js - Express - Typescript for my YouTube channel
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+    console.log("App is running on PORT 3000");
+});
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 /**
- * GraphQL - Node.js for my YouTube channel
+ * GraphQL - Node.js - Express - Typescript for my YouTube channel
  */
 
  import express = require("express");
 
  const app = express();
- 
+
  app.listen(3000, () => {
    console.log("App is running on PORT 3000");
  });
- 
